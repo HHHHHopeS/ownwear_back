e89097b6c9bcbcacc14747de2f318c99d7f325b1
disc0803 <88529803+disc0803@users.noreply.github.com>
2021-09-13 12:15:19 +0900

    09/13 test3

--- src/main/java/com/ownwear/app/OwnwearApplication.java
+++ src/main/java/com/ownwear/app/OwnwearApplication.java
@@ -2,7 +2,7 @@ package com.ownwear.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//s
 @SpringBootApplication
 public class OwnwearApplication {
 
