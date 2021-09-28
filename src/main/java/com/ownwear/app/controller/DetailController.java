d94d7f7d1da6434c046bbc4f9c32241b5cf45ffc
꼬낄라 <77871140+Braveleftji@users.noreply.github.com>
2021-09-28 16:52:08 +0900

    0928 1651 detailSave test

--- src/main/java/com/ownwear/app/controller/DetailController.java
+++ src/main/java/com/ownwear/app/controller/DetailController.java
@@ -8,10 +8,15 @@ import com.ownwear.app.model.Post;
 import com.ownwear.app.repository.CommentRepository;
 import com.ownwear.app.repository.PostRepository;
 import com.ownwear.app.vo.UserRelatedVo;
+import org.apache.tomcat.util.json.JSONParser;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -26,22 +31,42 @@ public class DetailController {
     private UserRepository userRepository;
 
     @GetMapping("/{postno}")
-    public PostVo getDetail(@PathVariable("postno") Long postno){
+    public PostVo getDetail(@PathVariable("postno") long postno){
 
         Optional<Post> byPostno = postRepository.findByPostno(postno);
+
         if (byPostno.isPresent()){
+
             Post post = byPostno.get();
+
+            Map<String, Object> imgdata = post.getImgdata();
+            imgdata= new HashMap<>();
+            imgdata.put("asd","Asd");
+            imgdata.put("assd","Assd");
+                    post.setImgdata(imgdata);
             System.out.println(userRepository.findById(post.getId()));
+
             String username = userRepository.findById(post.getId()).get().getUsername();
+
+            postRepository.save(post);
             int likecount = 1;
+
             ArrayList<HashTag> hashtags = null;
+
             ArrayList<UserRelatedVo> userRelated = null;
+
             ArrayList<Comment> comments = commentRepository.findByPostno(postno);
 
             PostVo postVo = new PostVo(post,likecount,hashtags,userRelated,comments,username);
+
             return postVo;
         }
         return null;
     }
 
+    @PostMapping("/save")
+    public PostVo savePost(@RequestBody PostVo postVo){
+
+        return postVo;
+    }
 }
--- src/main/java/com/ownwear/app/model/Post.java
+++ src/main/java/com/ownwear/app/model/Post.java
@@ -1,21 +1,33 @@
 package com.ownwear.app.model;
 
+import com.vladmihalcea.hibernate.type.json.JsonStringType;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.json.JSONObject;
+import org.json.JSONString;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 @Entity
 @Data
+@TypeDef(
+        name = "json",
+        typeClass = JsonStringType.class
+)
 public class Post {
     @Id
     @Column(name = "POSTNO")
     private long postno;
     private long id;
-    @Column(name = "IMGDATA", nullable = false, length = 4000)
-    private String imgdata;
+
+    @Column(name = "IMGDATA", columnDefinition = "json",nullable = true)
+    @Type(type = "json")
+    private Map<String , Object> imgdata = new HashMap<>();
     @CreationTimestamp
     @Column(name = "RDATE", nullable = false)
     private Timestamp rdate;
